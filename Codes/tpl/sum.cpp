844fb53e3e9fc047641aed9f352831bdafe8fafb
DavidingPlus <2065666169@qq.com>
2023-10-13 09:16:43 +0800

    update 10.13

--- Codes/tpl/sum.cpp
+++ Codes/tpl/sum.cpp
@@ -18,33 +18,32 @@ auto sum(int count, T v0...) {  // C++ style
     return s;
 }
 
-// 方法2，好像不行
-template <typename value_type, typename... Args>
-auto sum2(value_type val, Args... args) {
-    return val + sum2(args...);
-}
-
-// 特化，他不准函数模板进行特化，操！
-template <typename value_type>
-auto sum2<value_type>(value_type val) {
-    return val;
-}
-
-// 老白的写法，方法3
+// 老白的写法，方法2，没看懂
 template <typename... Args>
-auto sum3(Args... args) {
-    std::cout << sizeof...(args) << std::endl;
+auto sum2(Args... args) {
+    // std::cout << sizeof...(args) << std::endl;
 
     return (0 + ... + args);
 }
 
+// 方法3，出问题了
+template <typename value_type, typename... Args>
+auto sum3(value_type val, Args... args) {
+    return val + sum2(args...);
+}
+
+// 特化版本
+auto sum3() {
+    return 0;
+}
+
 int main() {
     // C风格的参数一般第一个参数都会给出参数包的个数，后面才是数据
-    std::cout << sum(4, 1, 2, 3, 4) << std::endl;
+    std::cout << sum(4, 1, 2, 3, 4, 5) << std::endl;
 
-    std::cout << sum2(1, 2, 3, 4) << std::endl;
+    std::cout << sum2(1, 2, 3, 4, 5) << std::endl;
 
-    std::cout << sum3(1, 2, 3, 4) << std::endl;
+    std::cout << sum3(1, 2, 3, 4, 5) << std::endl;
 
     return 0;
 }
