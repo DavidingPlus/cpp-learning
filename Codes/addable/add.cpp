4c92a4b2825df6074a1805465ad658f627b3eb2f
刘治学 <2065666169@qq.com>
2023-10-19 19:37:21 +0800

    update 10.19

--- Codes/addable/add.cpp
+++ Codes/addable/add.cpp
@@ -1,14 +1,14 @@
-//add.cpp
-
-#include <iostream>
+// add.cpp
 
 #include "add.h"
+
+#include <iostream>
 
 extern int add_wrapper(int, int);
 
 int main() {
     std::cout << add(1, 2) << std::endl;
-    std::cout << add_wrapper(3, 4) << std::endl; 
+    std::cout << add_wrapper(3, 4) << std::endl;
 
     return 0;
 }
