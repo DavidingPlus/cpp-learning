ca01de668ed36f969d14238fc9add747632670dd
刘治学 <2065666169@qq.com>
2023-10-19 20:32:56 +0800

    update 10.19

--- Codes/addable/add.cpp
+++ Codes/addable/add.cpp
@@ -9,6 +9,14 @@ extern int add_wrapper(int, int);
 int main() {
     std::cout << add(1, 2) << std::endl;
     std::cout << add_wrapper(3, 4) << std::endl;
+    std::cout << add(1.1, 2.2) << std::endl;
+
+    // 指针(error)
+    // int *p = nullptr, *q = nullptr;
+    // std::cout << add(p, q) << std::endl;
+
+    // bool(error)
+    // std::cout << add(true, false) << std::endl;
 
     return 0;
 }
