9be54583f062a4f434e890272c90160b7c801dfb
刘治学 <2065666169@qq.com>
2024-01-31 11:16:43 +0800

    update 1.31

--- Codes/一些知识点/简单单例模式/main.cpp
+++ Codes/一些知识点/简单单例模式/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 
-#include "sigleton.h"
+#include "singleton.h"
 
 int main() {
+    // Singleton s;  // error
+
+    Singleton *s1 = Singleton::getInstance();
+    Singleton *s2 = Singleton::getInstance();
+
+    s1->print();
+    s2->print();
+
     return 0;
 }
