ee2b4ec075b4221170bda1dc6c941746798826eb
DavidingPlus <davidingplus@qq.com>
2024-10-28 18:40:48 +0800

    customer producer demo

--- snippet/TemporaryTest/main.cpp
+++ snippet/TemporaryTest/main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
+#include <random>
+
+
+int genRandomNumber(int left, int right)
+{
+    static std::random_device rd;
+
+    static std::mt19937_64 generator(rd());
+
+    std::uniform_int_distribution<int> distribution(left, right);
+
+    return distribution(generator);
+}
 
 
 int main()
 {
-    std::cout << __cplusplus << std::endl;
+    for (int i = 0; i < 5; ++i) std::cout << genRandomNumber(1, 100) << ' ' << std::flush;
+
+    std::cout << std::endl;
 
 
     return 0;
